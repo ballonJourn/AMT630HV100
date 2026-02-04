e557f92ee38389fd423d78fab0d7fe5c03ac7b63
ouchunhua <ouchunhua@hcn2000.com>
2026-02-04 14:08:11 +0800

    [feat]仪表演示模式改为长按上键

--- amt630hV100-sdk-beta-20250625/amt630hv100-freertos/app/hcn/ui/HCN_DC001/src/view/view_manager.c
+++ amt630hV100-sdk-beta-20250625/amt630hv100-freertos/app/hcn/ui/HCN_DC001/src/view/view_manager.c
@@ -103,8 +103,8 @@ static void hcn_key_handle(uint8_t id)
 
     switch (id)
     {
-        case  SET_KEY_LONG_PR   :
-                // get_demonstration_state() ? demonstration_stop() : demonstration_start();  //取消演示模式
+        case  MODE_KEY_LONG_PR   :
+                get_demonstration_state() ? demonstration_stop() : demonstration_start();  //取消演示模式
                 break;
 
         case  BACK_KEY_SHORT_PR :
